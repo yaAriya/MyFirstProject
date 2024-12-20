f890389d7680c42ae9cdb2e5b226fc9975846d69
Yarochkina Ariya <yarochkina.ariya@gmail.com>
2024-12-20 22:43:09 +0300

    Deleting unnecessary files

--- src/com/beginning/Transport/Main.java
+++ src/com/beginning/Transport/Main.java
@@ -1,5 +1,7 @@
 package com.beginning.Transport;
 
+import com.sun.source.tree.ArrayAccessTree;
+
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -9,10 +11,7 @@ public class Main implements Cloneable {
         List <Plane> firstPlaneShowroom = new ArrayList();
         ArrayList<Car> secondCarShowroom = new ArrayList<>();
         ArrayList<Car>  thirdCarShowroom = new ArrayList<>();
-        String MyFirstName = "Hello, my name is Aria and I love programming";
-        //firstCarShowroom.add(new Car(4, 5, "black", "Audi", "petrol", 5, 4));
-        //firstCarShowroom.add(new Car(4, 85, "grey", "BMW", "diesel", 4, 2));
-        //firstCarShowroom.add(new Car(4, 300, "White", "Mercedes", "electricity", 5, 4));
+
         Car firstCar = new Car(4, 5, "black", "Audi", "petrol", 5, 4);
         Car secondCar =  new Car(4, 85, "grey", "BMW", "diesel", 4, 2);
         Car thirdCar = new Car(4, 300, "White", "Mercedes", "electricity", 5, 4);
@@ -20,13 +19,9 @@ public class Main implements Cloneable {
         firstCarShowroom.add(thirdCar);
         firstCarShowroom.add(secondCar);
 
-        //firstPlaneShowroom.add(new Plane(2, 390, "white", "Belavia", "petrol", 40, 2, 4, 4 ));
-        //firstPlaneShowroom.add(new Plane(2, 360, "blue", "Belavia", "petrol", 45, 2, 4, 4 ));
-        //firstPlaneShowroom.add(new Plane(2, 420, "yellow", "Belavia", "petrol", 50, 2, 4, 4 ));
         Plane firstPlane = new Plane(2, 390, "white", "Belavia", "petrol", 40, 2, 4, 4 );
         Plane secondPlane =  new Plane(2, 360, "blue", "Belavia", "petrol", 45, 2, 4, 4 );
         Plane thirdPlane = secondPlane.clone();
-       // Plane thirdPlane = new Plane(2, 420, "yellow", "Belavia", "petrol", 50, 2, 4, 4 );
         firstPlaneShowroom.add(firstPlane);
         firstPlaneShowroom.add(secondPlane);
         firstPlaneShowroom.add(thirdPlane);
