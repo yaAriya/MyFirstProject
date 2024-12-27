1b014ab38a54145455f569831055b0aa94924483
Yarochkina Ariya <yarochkina.ariya@gmail.com>
2024-12-27 18:24:00 +0300

    Changing the name of the objects being compared

--- src/com/beginning/Transport/MaxPowerComparator.java
+++ src/com/beginning/Transport/MaxPowerComparator.java
@@ -4,8 +4,8 @@ import java.util.Comparator;
 
 public class MaxPowerComparator implements Comparator<Car>{
     @Override
-    public int compare (Car o1, Car o2){
-        return o1.getPower() - o2.getPower();
+    public int compare (Car car1, Car car2){
+        return car1.getPower() - car2.getPower();
     }
 
 }
