d3daf1d264ab442b346c105b5ebd675f477dac73
Yarochkina Ariya <yarochkina.ariya@gmail.com>
2024-12-27 18:01:45 +0300

    Creating a Transport Initializer and adding data there

--- src/com/beginning/Transport/Main.java
+++ src/com/beginning/Transport/Main.java
@@ -1,29 +1,18 @@
 package com.beginning.Transport;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-public class Main implements Cloneable {
+public class Main {
     public static void main(String[] args) throws CloneNotSupportedException{
-        List <Car> firstCarShowroom = new ArrayList();
-        List <Plane> firstPlaneShowroom = new ArrayList();
-        ArrayList<Car> secondCarShowroom = new ArrayList<>();
-        ArrayList<Car>  thirdCarShowroom = new ArrayList<>();
-
-        Car firstCar = new Car(4, 5, "black", "Audi", "petrol", 5, 4);
-        Car secondCar =  new Car(4, 85, "grey", "BMW", "diesel", 4, 2);
-        Car thirdCar = new Car(4, 300, "White", "Mercedes", "electricity", 5, 4);
-        firstCarShowroom.add(firstCar);
-        firstCarShowroom.add(thirdCar);
-        firstCarShowroom.add(secondCar);
-
-        Plane firstPlane = new Plane(2, 390, "white", "Belavia", "petrol", 40, 2, 4, 4 );
-        Plane secondPlane =  new Plane(2, 360, "blue", "Belavia", "petrol", 45, 2, 4, 4 );
-        Plane thirdPlane = secondPlane.clone();
-        firstPlaneShowroom.add(firstPlane);
-        firstPlaneShowroom.add(secondPlane);
-        firstPlaneShowroom.add(thirdPlane);
-
+        TransportInitializer transportInitializer = new TransportInitializer();
+        List <Plane> firstPlaneShowroom = transportInitializer.initializerPlane();
+        for(int i =0; i<firstPlaneShowroom.size(); i++) {
+            System.out.println("Первый массив самолетов " + (i+1) + ": " +  transportInitializer.initializerPlane() );
+        }
+        List <Car> firstCarShowroom = transportInitializer.initializerCar();
+        for (int i=0; i<firstPlaneShowroom.size(); i ++) {
+            System.out.println("Первый массив машин "+(i+1)+ ": "+ transportInitializer.initializerCar());
+        }
 
         Comparator <Car> powerComparator = new MaxPowerComparator();
         firstCarShowroom.sort(powerComparator);
---
+++ src/com/beginning/Transport/TransportInitializer.java
@@ -0,0 +1,34 @@
+package com.beginning.Transport;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransportInitializer implements Cloneable {
+    public List <Car> initializerCar(){
+        List<Car> firstCarShowroom = new ArrayList();
+
+        Car firstCar = new Car(4, 5, "black", "Audi", "petrol", 5, 4);
+        Car secondCar =  new Car(4, 85, "grey", "BMW", "diesel", 4, 2);
+        Car thirdCar = new Car(4, 300, "White", "Mercedes", "electricity", 5, 4);
+
+        firstCarShowroom.add(firstCar);
+        firstCarShowroom.add(thirdCar);
+        firstCarShowroom.add(secondCar);
+
+        return firstCarShowroom;
+    }
+    public List <Plane> initializerPlane() throws CloneNotSupportedException{
+        List <Plane> firstPlaneShowroom = new ArrayList();
+
+        Plane firstPlane = new Plane(2, 390, "white", "Belavia", "petrol", 40, 2, 4, 4 );
+        Plane secondPlane =  new Plane(2, 360, "blue", "Belavia", "petrol", 45, 2, 4, 4 );
+        Plane thirdPlane = secondPlane.clone();
+
+        firstPlaneShowroom.add(firstPlane);
+        firstPlaneShowroom.add(secondPlane);
+        firstPlaneShowroom.add(thirdPlane);
+
+        return firstPlaneShowroom;
+    }
+
+}
